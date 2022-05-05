aae131973c056dc7ff1eb6fa74b17da380053fef
MukundaSaiChaitanyaThota <91048236+MukundaSaiChaitanyaThota@users.noreply.github.com>
2022-05-06 00:50:27 +0530

    Update tddbucketstest.py

--- tddbucketstest.py
+++ tddbucketstest.py
@@ -1,10 +1,15 @@
-import Driven_Range
+import tddbuckets
 import unittest
 
 class driven_range_test(unittest.TestCase):
 
     def test_rangeAndFrequency(self):
-        self.assertTrue(driven_range().main([4,5]) == {'4-5': '2'})
+        self.assertTrue(driven_range().main([1,3,2,3]) == {'1-3': '4'})
+        self.assertTrue(driven_range().main([2,3,4,5,6,7,,8,10]) == {'2-8': '7', '10-10': '1'})
+        self.assertTrue(driven_range().main([-5,-4,-3,1,2,3,4,6,7,8]) == {'-5--3': '3', '1-4': '4', '6-8': '3'})
+        self.assertTrue(driven_range().main([2,2,4,4,10,11,12]) == {'2-4': '4', '10-12': '3'})
+        self.assertTrue(driven_range().main([3,4]) == {'3-4': '2'})
+
 
 if __name__ == '__main__':
   unittest.main()
